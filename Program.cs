2241de0ce23a2661036dd6f7267110a1e4ffbacf
MatiBravo47 <matibravoneron@gmail.com>
2024-04-28 01:21:14 -0300

    agrego mas ejercicios guia 1

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.CodeDom.Compiler;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -89,6 +90,105 @@ namespace guia1Variables
             metrosPersona = double.Parse(Console.ReadLine());
             pulgadasPersona = metrosPersona * 39.37;
             Console.WriteLine("El equivalente a pulgadas es " + pulgadasPersona + " pulgadas");
+
+            //7)
+            //Calculo de IMC(Indice de masa corporal)
+            //Ingresa el peso en kilogramos y la altura en metros de una persona. Calcula su IMC y muestra el resultado.
+            double pesoKilos, alturaMetros, imc;
+            
+            Console.WriteLine("Ingrese el peso en kilos");
+            pesoKilos = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la altura en metros");
+            alturaMetros = double.Parse(Console.ReadLine());
+
+            imc = (pesoKilos / (alturaMetros * alturaMetros)) ;
+
+            Console.WriteLine("El IMC es: " +  imc);
+
+            //8)
+            //Edad de mascotas 
+            //Ingresa la edad en anios de una mascota y muestra su edad 
+            //equivalente en anios de humano (considerando una escala de
+            ////un anio de mascota = 7 anios de humano)
+
+            int aniosHumanos;
+
+            Console.WriteLine("Ingrese la edad de la mascota");
+            aniosHumanos = int.Parse(Console.ReadLine()) * 7;
+            Console.WriteLine("La mascota tiene un equivalente a " + aniosHumanos + " anios humanos");
+
+            //9)Tiempo de viaje
+            //Ingresa la distancia en kilometros y la velocidad en kilometros por hora
+            //para un viaje.
+            //Calcula el tiempo total del viaje en horas.
+
+            double distanciaKM, velocidadKM;
+
+            Console.WriteLine("Ingrese la distancia en kilometros");
+            distanciaKM = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la velocidad en kilometros");
+            velocidadKM = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("El tiempo total es de " + distanciaKM / velocidadKM + " horas");
+
+            //11)
+            //Gastos de una salida:
+            //Ingresa el costo de la entrada de cine, el costo de la cena
+            //y el costo del transporte para una salida. Calcula el gasto total
+            //y muestra el resultado.
+
+            int costoCine, costoCena, costoTransporte, costoTotal;
+            Console.WriteLine("Ingrese el valor de la entrada");
+            costoCine = int.Parse( Console.ReadLine());
+            Console.WriteLine("Ingrese el costo de la cena");
+            costoCena = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el costo del transporte");
+            costoTransporte = int.Parse(Console.ReadLine());
+            costoTotal = costoCine + costoCena + costoTransporte;
+            Console.WriteLine("El costo total es: " + costoTotal);
+
+            //12)
+            //Promedio de tres calificaciones:
+            //Ingresa tres calificaciones y calcula el promedio de las mismas
+
+            int nota1, nota2, nota3;
+
+            Console.WriteLine("Ingrese la nota 1");
+            nota1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la nota 2");
+            nota2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la nota 3");
+            nota3 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("El promedio de las notas es: " + ((nota1 + nota2 + nota3) / 3));
+
+            //13)Perimetro de un triangulo:
+            //Ingresa las longitudes de los tres lados de un triangulo
+            //y calcula su perimetro.
+            double longitud1, longitud2, longitud3;
+            Console.WriteLine("Ingrese longitud lado 1");
+            longitud1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese longitud lado 2");
+            longitud2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese longitud lado 3");
+            longitud3 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("El perimetro del triangulo es:" + (longitud1 + longitud2 + longitud3));
+
+            //14)Cantidad de dolares en pesos argentinos.
+            //Ingresa la cotizacion del dolar y la cantidad de dolares a convertir
+            //Convierte la cantidad de dolares ingresada a pesos argentinos.
+
+
+            //15)Area de un circulo
+            //Ingresa el radio de un circulo y calcula su area.
+
+
+
+
+
+
 
             Console.ReadLine();
         }
